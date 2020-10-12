1d107c602d2288b03c070ecb6f92bb3c50dd16c4
Richard Deloge <richarddeloge@gmail.com>
2020-10-12 19:19:56 +0200

    Complete tests for php8 and union with bowl

---
+++ tests/Bowl/BowlWithUnionTest.php
@@ -0,0 +1,125 @@
+<?php
+
+/**
+ * Recipe.
+ *
+ * LICENSE
+ *
+ * This source file is subject to the MIT license and the version 3 of the GPL3
+ * license that are bundled with this package in the folder licences
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to richarddeloge@gmail.com so we can send you a copy immediately.
+ *
+ *
+ * @copyright   Copyright (c) 2009-2020 Richard Déloge (richarddeloge@gmail.com)
+ *
+ * @link        http://teknoo.software/recipe Project website
+ *
+ * @license     http://teknoo.software/license/mit         MIT License
+ * @author      Richard Déloge <richarddeloge@gmail.com>
+ */
+
+namespace Teknoo\Tests\Recipe\Bowl;
+
+use Teknoo\Recipe\Bowl\Bowl;
+use Teknoo\Recipe\Bowl\BowlInterface;
+
+/**
+ * @copyright   Copyright (c) 2009-2020 Richard Déloge (richarddeloge@gmail.com)
+ *
+ * @link        http://teknoo.software/recipe Project website
+ *
+ * @license     http://teknoo.software/license/mit         MIT License
+ * @author      Richard Déloge <richarddeloge@gmail.com>
+ *
+ * @covers \Teknoo\Recipe\Bowl\Bowl
+ * @covers \Teknoo\Recipe\Bowl\BowlTrait
+ */
+class BowlWithUnionTest extends AbstractBowlTest
+{
+    protected function getCallable()
+    {
+        $code = <<<'EOF'
+return function (
+    Teknoo\Recipe\ChefInterface $chef, 
+    string $bar, 
+    $bar2, 
+    $foo2, 
+    \DateTimeImmutable|\DateTime $date, 
+    $_methodName
+) {
+    $chef->continue([
+        'bar' => $bar,
+        'bar2' => $bar,
+        'foo2' => $foo2,
+        'date' => $date->getTimestamp(),
+        '_methodName' => $_methodName,
+    ]);
+};
+EOF;
+
+        return eval($code);
+    }
+
+    protected function getMapping()
+    {
+        return ['bar' => 'foo', 'bar2' => ['bar', 'foo']];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function buildBowl(): BowlInterface
+    {
+        return new Bowl(
+            $this->getCallable(),
+            $this->getMapping(),
+            'bowlClass'
+        );
+    }
+
+    public function testExceptionOnExecuteWithBadChef()
+    {
+        if (PHP_VERSION_ID < 80000) {
+            self::markTestSkipped('Only for PHP 8');
+
+            return;
+        }
+
+        parent::testExceptionOnExecuteWithBadChef();
+    }
+
+    public function testExceptionOnExecuteWithBadWorkPlan()
+    {
+        if (PHP_VERSION_ID < 80000) {
+            self::markTestSkipped('Only for PHP 8');
+
+            return;
+        }
+
+        parent::testExceptionOnExecuteWithBadWorkPlan();
+    }
+
+    public function testExecute()
+    {
+        if (PHP_VERSION_ID < 80000) {
+            self::markTestSkipped('Only for PHP 8');
+
+            return;
+        }
+
+        parent::testExecute();
+    }
+
+    public function testExceptionWhenExecuteAndMissingAndIngredientInWorkPlan()
+    {
+        if (PHP_VERSION_ID < 80000) {
+            self::markTestSkipped('Only for PHP 8');
+
+            return;
+        }
+
+        parent::testExceptionWhenExecuteAndMissingAndIngredientInWorkPlan();
+    }
+}
